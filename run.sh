beff6a7a2ad1bb25581e96432ba8b7b48788f46d
Menzel, Michael <m.menzel@sap.com>
2015-07-23 17:45:01 +0200

    super cool, can do everything version

--- run.sh
+++ run.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
-export SSL_CERT_FILE=/home/user/path-to-your-ca-file.crt
-ansible-playbook example/site.yml -i example/hosts -vvvv -f 20
+current_folder=`pwd`
+export SSL_CERT_FILE=$current_folder/ca-bundle.crt
+
+ansible-playbook create_instances_gce/site.yml -i example/hosts -vvvv -f 5
+#ansible-playbook example/site.yml -i example/hosts -vvvv -f 20
