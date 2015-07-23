ddba26fc54b49e8a76312d1d88ce45e67f137815
Menzel, Michael <m.menzel@sap.com>
2015-07-23 17:24:02 +0200

    improved example added google cert file env var example to run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-ansible-playbook example/site.yml -i example/hosts -vvvv
+export SSL_CERT_FILE=/home/user/path-to-your-ca-file.crt
+ansible-playbook example/site.yml -i example/hosts -vvvv -f 20
