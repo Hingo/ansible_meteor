298ec9afd9b45f1b811ca8cc82fea76049df23fb
Menzel, Michael <m.menzel@sap.com>
2015-07-24 13:24:50 +0200

    create and delete working

--- run.sh
+++ run.sh
@@ -2,6 +2,9 @@
 
 current_folder=`pwd`
 export SSL_CERT_FILE=$current_folder/ca-bundle.crt
+export PYTHONPATH=/usr/local/lib/python2.7/site-packages/
 
-ansible-playbook create_instances_gce/site.yml -i example/hosts -vvv -f 5
+ansible-playbook create_instances_gce/site.yml -i ./gce.py -f 5
+ansible-playbook delete_instances_gce/site.yml -i ./gce.py -f 5
+
 #ansible-playbook example/site.yml -i example/hosts -vvvv -f 20
