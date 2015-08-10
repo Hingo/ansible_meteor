342d3fa11779cdda06fe2662ed7dfcf1298544f0
Menzel, Michael <m.menzel@sap.com>
2015-08-10 11:42:02 +0200

    further improved gce orchestration gce pretty much sucks -> documentation sparse

---
+++ ansible/start.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+current_folder=`pwd`
+export SSL_CERT_FILE=$current_folder/ca-bundle.crt
+export ANSIBLE_HOST_KEY_CHECKING=False
+export PYTHONPATH=/usr/local/lib/python2.7/site-packages/
+
+ansible-playbook create_instances_gce/site.yml -i ./inventory/ -f 5 -vvvv
+
+#ansible-playbook example/site.yml -i example/hosts -vvvv -f 20
--- ansible/stop.sh
+++ ansible/stop.sh
@@ -2,9 +2,9 @@
 
 current_folder=`pwd`
 export SSL_CERT_FILE=$current_folder/ca-bundle.crt
+export ANSIBLE_HOST_KEY_CHECKING=False
 export PYTHONPATH=/usr/local/lib/python2.7/site-packages/
 
-ansible-playbook create_instances_gce/site.yml -i ./gce.py -f 5
-ansible-playbook delete_instances_gce/site.yml -i ./gce.py -f 5
+ansible-playbook delete_instances_gce/site.yml -i ./inventory/ -f 5
 
 #ansible-playbook example/site.yml -i example/hosts -vvvv -f 20
