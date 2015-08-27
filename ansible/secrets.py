aa89471ad45f1d5b9dd0362c8b46422c2ee00308
Menzel, Michael <m.menzel@sap.com>
2015-08-27 15:11:37 +0200

    further tried to establish SSH connection to GCE instances

---
+++ ansible/secrets.py
@@ -0,0 +1,3 @@
+GCE_PARAMS = ('547143891756-08ihrtbkhvvas21qm88kvbhv7i47mv87@developer.gserviceaccount.com', '/Users/d046179/git/ansible_meteor/ansible/unfancystuff-ed917d3b473a.pem')
+
+GCE_KEYWORD_PARAMS = {'project': 'fancystuff-11'}
--- ansible/start.sh
+++ ansible/start.sh
@@ -3,7 +3,7 @@
 current_folder=`pwd`
 export SSL_CERT_FILE=$current_folder/ca-bundle.crt
 export ANSIBLE_HOST_KEY_CHECKING=False
-export PYTHONPATH=/usr/local/lib/python2.7/site-packages/
+export PYTHONPATH=$PYTHONPATH:./inventory/:/usr/local/lib/python2.7/site-packages/
 
 ansible-playbook create_instances_gce/site.yml -i ./inventory/ -f 5 -vvvv
 
