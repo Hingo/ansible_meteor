c8712c0a66141ebd76a7e7397f40dbc590270c92
Menzel, Michael <m.menzel@sap.com>
2015-08-31 18:31:05 +0200

    updated site.yml for example 1 -> meteor added first version of slide deck

--- ansible/aws/stop-aws.sh
+++ ansible/aws/stop-aws.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 export ANSIBLE_HOST_KEY_CHECKING=False
-`ansible-vault view register_aws_creds.sh`
-ansible-playbook create_instances_aws/site.yml -e state=absent -i ./inventory/ --private-key vineet-michael-ansible.pem --ask-vault-pass -vvvv
+
+`ansible-vault view aws.creds`
+ansible-playbook destroy_instances_aws/site.yml -e state=absent -i ./inventory/ --private-key vineet-michael-ansible.pem -vvvv
