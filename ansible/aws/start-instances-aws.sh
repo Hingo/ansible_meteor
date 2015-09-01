9056e257821886c9b92d8805b0d8ea40c33d6b3a
Menzel, Michael <m.menzel@sap.com>
2015-09-01 14:28:19 +0200

    improved slides improved ansible code

--- ansible/aws/start-instances-aws.sh
+++ ansible/aws/start-instances-aws.sh
@@ -2,5 +2,6 @@
 
 export ANSIBLE_HOST_KEY_CHECKING=False
 
+echo "Please enter the ansible-vault password to decrypt your AWS credentials:"
 `ansible-vault view aws.creds`
-ansible-playbook create_instances_aws/site.yml -e state=present -i ./inventory/ --private-key vineet-michael-ansible.pem -vvvv
+ansible-playbook create_instances_aws/site.yml -e state=present -i ./inventory/ --private-key vineet-michael-ansible.pem
--- ansible/aws/stop-instances-aws.sh
+++ ansible/aws/stop-instances-aws.sh
@@ -2,5 +2,6 @@
 
 export ANSIBLE_HOST_KEY_CHECKING=False
 
+echo "Please enter the ansible-vault password to decrypt your AWS credentials:"
 `ansible-vault view aws.creds`
-ansible-playbook destroy_instances_aws/site.yml -e state=absent -i ./inventory/ --private-key vineet-michael-ansible.pem -vvvv
+ansible-playbook destroy_instances_aws/site.yml -e state=absent -i ./inventory/ --private-key vineet-michael-ansible.pem
