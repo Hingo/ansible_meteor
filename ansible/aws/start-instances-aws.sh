58bafc3dc15eb896b15ef6ee6545fc6617cc0c60
Menzel, Michael <m.menzel@sap.com>
2015-09-02 09:12:05 +0200

    finished ansible setup improved architecture drawings

--- ansible/aws/start-instances-aws.sh
+++ ansible/aws/start-instances-aws.sh
@@ -3,9 +3,14 @@
 export ANSIBLE_HOST_KEY_CHECKING=False
 
 echo ""
+echo "SIGNAL"
+echo "launching automatron... putting on ec2 jacket..."
+echo ""
+echo ""
 echo "****************************************************************************"
 echo "* Please enter the ansible-vault password to decrypt your AWS credentials: *"
 echo "****************************************************************************"
 echo ""
 `ansible-vault view aws.creds`
+echo ""
 ansible-playbook create_instances_aws/site.yml -e state=present -i ./inventory/ --private-key vineet-michael-ansible.pem
--- ansible/aws/stop-asg-aws.sh
+++ ansible/aws/stop-asg-aws.sh
@@ -3,9 +3,14 @@
 export ANSIBLE_HOST_KEY_CHECKING=False
 
 echo ""
+echo "SIGNAL"
+echo "launching automatron... putting on ec2 jacket..."
+echo ""
+echo ""
 echo "****************************************************************************"
 echo "* Please enter the ansible-vault password to decrypt your AWS credentials: *"
 echo "****************************************************************************"
 echo ""
 `ansible-vault view aws.creds`
+echo ""
 ansible-playbook destroy_asg_aws/site.yml -e state=absent -i ./inventory/ --private-key vineet-michael-ansible.pem
